a40189c16320755f5ba4fe3b648aef6e334233fc
Elias Champi <eliaschampi@protonmail.com>
2022-09-14 19:23:02 +0000

    Add name variable

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 #Aqui mostramos en la pantalla el texto de bienvenida
 print("Hola! Bienvenido a mi trivia")
+
+#Obtenemos el nombre aqui
+name = input("Me podrías decir como quieres que te llame\nNombre:")
